f55c959f72b6011e38b9d02175d9f0eb6861fd6f
renandsb <renandsb@gmail.com>
2021-02-02 18:42:01 -0300

    first-commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CategoriesModule } from './pages/categories/categories.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CategoriesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/pages/categories/categories-routing.module.ts
@@ -0,0 +1,16 @@
+import { CategoriesListComponent } from './categories-list/categories-list.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { CategoriesFormComponent } from './categories-form/categories-form.component';
+
+const routes: Routes = [
+  {path: '', component: CategoriesListComponent},
+  {path: 'new', component: CategoriesFormComponent},
+  {path: ':id/edit', component: CategoriesFormComponent},
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class CategoriesRoutingModule { }
